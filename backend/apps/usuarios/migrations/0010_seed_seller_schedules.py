26fc0c33cd49da9d1ab4ead3f2c65703cbcb06a8
MAURICIO <andresmau1126@gmail.com>
2026-09-21 10:19:17 -0500

    fix: completar datos de horarios en migracion

--- backend/apps/usuarios/migrations/0010_seed_seller_schedules.py
+++ backend/apps/usuarios/migrations/0010_seed_seller_schedules.py
@@ -10,12 +10,12 @@ class Migration(migrations.Migration):
         migrations.RunSQL(
             sql="""
                 DELETE FROM seller_schedules;
-                INSERT INTO seller_schedules (usuario_id, name, start_time, end_time, is_active)
-                SELECT id, 'Mauricio', '08:00', '12:00', TRUE
+                INSERT INTO seller_schedules (usuario_id, name, start_time, end_time, is_active, nit)
+                SELECT id, 'Mauricio', '08:00', '12:00', TRUE, '1020085627-1'
                 FROM usuarios
                 WHERE email = 'andresmau.colamericano7b@gmail.com';
-                INSERT INTO seller_schedules (usuario_id, name, start_time, end_time, is_active)
-                SELECT id, 'José', '12:00', '19:00', TRUE
+                INSERT INTO seller_schedules (usuario_id, name, start_time, end_time, is_active, nit)
+                SELECT id, 'José', '12:00', '19:00', TRUE, '1020085627-1'
                 FROM usuarios
                 WHERE email = 'yo1126top76f@gmail.com';
             """,
